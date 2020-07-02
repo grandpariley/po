18224b4d952caff9a2e46624662d0720489b5347
ri-ri herman <rileyherman@tuta.io>
2020-07-02 17:49:47 -0600

    some latex commands for the runner

--- main.sh
+++ main.sh
@@ -10,5 +10,18 @@ case $i in
         # /usr/bin/time -v python3 main.py
         python3 main.py
     ;;
+    proposal)
+        cd proposal
+        pdflatex proposal.tex
+        cd -
+    ;;
+    report)
+        cd report
+        pdflatex report.tex
+        cd -
+    ;;
+    clean-tex)
+        rm proposal/*.aux proposal/*.log report/*.aux report/*.log
+    ;;
 esac
 done

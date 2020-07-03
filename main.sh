c3bf03bed594b3f884e75ae4fd1b85f8bb986dc5
ri-ri herman <rileyherman@tuta.io>
2020-07-02 22:10:48 -0600

    support bibliographies for tex

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function clean-tex {
+    rm -rf proposal/*.aux proposal/*.log proposal/*.blg proposal/*.bbl proposal/*.bcf proposal/*.xml report/*.aux report/*.log report/*.blg report/*.bbl report/*.bcf report/*.xml
+}
+
 for i
 do
 case $i in 
@@ -13,15 +17,21 @@ case $i in
     proposal)
         cd proposal
         pdflatex proposal.tex
+        biber proposal
+        pdflatex proposal.tex
         cd -
+        clean-tex
     ;;
     report)
         cd report
         pdflatex report.tex
+        biber report
+        pdflatex report.tex
         cd -
+        clean-tex
     ;;
     clean-tex)
-        rm proposal/*.aux proposal/*.log report/*.aux report/*.log
+        clean-tex    
     ;;
 esac
 done

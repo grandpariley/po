06450b8fcd39feaca008a8edff090a58081d3b39
ri-ri herman <rileyherman@tuta.io>
2020-07-23 21:14:27 -0600

    start dat spea2

--- pkg/consts.py
+++ pkg/consts.py
@@ -2,4 +2,7 @@ class Constants():
     #define PSO constants
     PSO_SWARM_SIZE = 30
     PSO_MAX_ITERATIONS = 100
-    
+    #define SPEA2 Constants
+    SPEA2_INITIAL_POPULATION = 30
+    SPEA2_MAX_ARCHIVE_SIZE = 30
+    SPEA2_MAX_GENERATIONS = 100
--- pkg/spea2/spea2.py
+++ pkg/spea2/spea2.py
@@ -1,5 +1,34 @@
+import copy
 from pkg.problem.solver import Solver
+from pkg.consts import Constants
 
 class Spea2(Solver):
+    def calculate_fitness(self, population):
+        pass
+
+    def get_non_dominated(self, population):
+        return [], []
+
+    def truncate(self, population):
+        return []
+
+    def binary_tournament_selection(self, population):
+        return []
+
+    def solve_helper(self, population, archive, generation):
+        self.calculate_fitness(population)
+        self.calculate_fitness(archive)
+        non_dominated, dominated = self.get_non_dominated(population + archive)
+        new_archive = copy.deepcopy(non_dominated)
+        while len(new_archive) != Constants.SPEA2_MAX_ARCHIVE_SIZE:
+            if len(new_archive) > Constants.SPEA2_MAX_ARCHIVE_SIZE:
+                new_archive = self.truncate(new_archive)
+            elif len(new_archive) < Constants.SPEA2_MAX_ARCHIVE_SIZE:
+                new_archive = new_archive + dominated
+        generation += 1
+        if generation == Constants.SPEA2_MAX_GENERATIONS:
+            return self.get_non_dominated(new_archive)[0]
+        return self.solve_helper(self.binary_tournament_selection(population), new_archive, generation)
+
     def solve(self):
         return self.problem

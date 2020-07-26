8470dd56ac9e13c3bb17320344291e28427fb209
rileyherman <riley.herman@fcc-fac.ca>
2020-07-26 09:24:00 -0600

    commit

--- pkg/consts.py
+++ pkg/consts.py
@@ -2,7 +2,6 @@ class Constants():
     #define PSO constants
     PSO_SWARM_SIZE = 30
     PSO_MAX_ITERATIONS = 100
-<<<<<<< HEAD
     #define NSGA2 constants
     NSGA2_NUM_INDIVIDUALS = 30
     NSGA2_NUM_GENERATIONS = 100
@@ -14,10 +13,8 @@ class Constants():
     FP_MAX_GENERATIONS = 100
     FP_NUMBER_OF_FLOWERS = 100
     FP_SWITCH_PROBABILITY = 0.5
-=======
     #define Bee Colony constraints
     BC_POPULATION_SIZE = 30
     BC_MAX_CYCLE_NUMBER = 100
     BC_LIMIT = 50
     
->>>>>>> bee-colony

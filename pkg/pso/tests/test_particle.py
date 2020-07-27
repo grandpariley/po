da1759f2a9a8737f6393793418a445c5c65e9591
rileyherman <riley.herman@fcc-fac.ca>
2020-07-27 16:14:31 -0600

    good ol tests

--- pkg/pso/tests/test_particle.py
+++ pkg/pso/tests/test_particle.py
@@ -1,40 +1,17 @@
-import unittest
-import random
-import copy
+import unittest, random, copy
 import unittest.mock as mock
+from pkg.random.random import Random
+from pkg.problem.tests.default_problems import default_consistent_problem_set_values
 from pkg.pso.particle import Particle, DRAG, SOCIAL_SCALE, COGNITIVE_SCALE
-from pkg.problem.problem import Problem
-from pkg.problem.variable import Variable
-from pkg.problem.constraint import Constraint
+
 
 
 class ParticleTest(unittest.TestCase):
-    def defaultVariables(self):
-        return [
-            Variable([0, 1, 2, 3]),
-            Variable([0, 1, 2, 3]),
-            Variable([0, 1, 2, 3]),
-        ]
-
-    def defaultConsistentProblem(self):
-        variables = self.defaultVariables()
-        variables[0].set_value(2)
-        variables[1].set_value(1)
-        variables[2].set_value(2)
-        return Problem(
-            variables,
-            [
-                Constraint((0, 2),
-                           lambda variables: variables[0] == variables[1]),
-                Constraint(tuple([1]), lambda variables: variables[0] == 1),
-                Constraint(tuple([2]), lambda variables: variables[0] > 0)
-            ], [lambda variables: v.get_value() for v in variables])
-
-    def defaultParticle(self):
-        return Particle(self.defaultConsistentProblem())
+    def default_particle(self):
+        return Particle(default_consistent_problem_set_values())
 
     def test_move(self):
-        particle = self.defaultParticle()
+        particle = self.default_particle()
         old_position = particle.problem.objective_values()
         particle.velocity = [1, 1, 1]
         v = particle.velocity
@@ -44,21 +21,29 @@ class ParticleTest(unittest.TestCase):
         self.assertEqual(old_position[1] + v[1], new_position[1])
         self.assertEqual(old_position[2] + v[2], new_position[2])
 
-    # TODO
     def test_accelerate(self):
-        with mock.patch('random.uniform', lambda dont, care : 0.5):
-            particle = self.defaultParticle()
-            old_particle = copy.deepcopy(particle)
-            particle.accelerate()
-            new_velocity = particle.velocity
-            calculated_velocity = [
-                (DRAG * old_particle.velocity[i]) + (SOCIAL_SCALE * random.uniform(0.0, 1.0) * (old_particle.best.get_value(i) - old_particle.problem.get_value(i))) + (COGNITIVE_SCALE * random.uniform(0.0, 1.0) * (old_particle.best.get_value(i) - old_particle.problem.get_value(i)))
-                for i in range(3)
-            ]
-            self.assertEqual(calculated_velocity[0], new_velocity[0])
-            self.assertEqual(calculated_velocity[1], new_velocity[1])
-            self.assertEqual(calculated_velocity[2], new_velocity[2])
+        Random.begin_test()
+        for _ in range(6):
+            Random.set_test_value_for("random_float_between_0_and_1", 0.5)
+        particle = self.default_particle()
+        old_particle = copy.deepcopy(particle)
+        particle.accelerate()
+        new_velocity = particle.velocity
+        calculated_velocity = [
+            (DRAG * old_particle.velocity[i]) + (SOCIAL_SCALE * Random.random_float_between_0_and_1() * (old_particle.best.get_value(i) - old_particle.problem.get_value(i))) + (COGNITIVE_SCALE * Random.random_float_between_0_and_1() * (old_particle.best.get_value(i) - old_particle.problem.get_value(i)))
+            for i in range(3)
+        ]
+        self.assertEqual(calculated_velocity[0], new_velocity[0])
+        self.assertEqual(calculated_velocity[1], new_velocity[1])
+        self.assertEqual(calculated_velocity[2], new_velocity[2])
+        Random.end_test()
 
-    # TODO
     def test_update_best(self):
-        pass
+        particle = Particle(default_consistent_problem_set_values())
+        better_problem = default_consistent_problem_set_values()
+        better_problem.set_value(0, 3)
+        better_problem.set_value(2, 3)
+        self.assertEqual(str(particle.problem), str(particle.get_best()))
+        particle.problem = better_problem
+        particle.update_best()
+        self.assertEqual(str(particle.best), str(better_problem))

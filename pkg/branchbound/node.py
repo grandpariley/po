9f1cd27fe8a9165f4a0b4976757537b64ab04365
rileyherman <riley.herman@fcc-fac.ca>
2020-07-28 16:49:05 -0600

    add those damn files

---
+++ pkg/branchbound/node.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def is_leaf(self):
+        return self.problem.all_assigned()
+
+    def is_consistent(self):
+        return self.problem.consistent()
+
+    def get_problem(self):
+        return self.problem
+   
+    def num_variables(self):
+        return self.problem.num_variables()
+   
+    def get_value(self, variable_index):
+        return self.problem.get_value(variable_index)
+   
+    def get_domain(self, variable_index):
+        return self.problem.get_domain(variable_index)
+   
+    def set_value(self, variable_index, variable_value):
+        return self.problem.set_value(variable_index, variable_value)
+
+    def get_objective_values(self):
+        return self.problem.objective_values()

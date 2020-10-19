b6d49bb6ace1f31749b15f67a5c1152d3afae3ca
rileyherman <riley.herman@fcc-fac.ca>
2020-10-18 18:03:27 -0600

    remove unfinished bee colony implementation

--- pkg/beecolony/bee.py
+++
@@ -1,6 +0,0 @@
-class Bee:
-    def __init__(self, problem):
-        self.problem = problem
-
-    def get_problem(self):
-        return self.problem
--- pkg/beecolony/food_source_memory.py
+++
@@ -1,8 +0,0 @@
-# TODO
-class FoodSourceMemory:
-    def __init__(self):
-        self.matrix = [[]]
-        self.generate_matrix()
-
-    def generate_matrix(self):
-        pass

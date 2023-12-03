b4bfdccb77ca7377f9d749e007c336132213fc3c
grandpariley <rileyherman@duck.com>
2023-12-03 17:45:23 -0600

    moead fixes I think

--- pkg/moead/family.py
+++ pkg/moead/family.py
@@ -2,20 +2,20 @@ from pkg.moead.individual import Individual
 from pkg.random.random import Random
 
 
-def generate_child(parent_population, data):
-    mum, dad = get_parents(parent_population)
+def generate_child(dating_pool, data):
+    mum, dad = get_parents(dating_pool)
     son = Individual(individual=dad)
     son.swap_half_genes(mum, data)
     son.emo_phase(data)
     return son
 
 
-def get_parents(parent_population):
-    index_mum = Random.random_int_between_a_and_b(0, len(parent_population) - 1)
+def get_parents(dating_pool):
+    index_mum = Random.random_int_between_a_and_b(0, len(dating_pool) - 1)
     index_dad = index_mum
     while index_mum == index_dad:
-        index_dad = Random.random_int_between_a_and_b(0, len(parent_population) - 1)
+        index_dad = Random.random_int_between_a_and_b(0, len(dating_pool) - 1)
     return (
-        parent_population[index_mum],
-        parent_population[index_dad]
+        dating_pool[index_mum],
+        dating_pool[index_dad]
     )
--- pkg/moead/moead.py
+++ pkg/moead/moead.py
@@ -8,42 +8,33 @@ from pkg.moead.sort import euclidean_distance_mapping
 from pkg.problem.solver import Solver
 
 
-def refresh_ep(ep, y):
-    dominated_by_y = []
-    for e in ep:
-        if y.does_dominate(e):
-            dominated_by_y.append(e)
-    ep = ep.difference(dominated_by_y)
-    ep.add(y)
-    for e in ep:
-        if e.does_dominate(y):
-            ep.remove(y)
-            break
+def is_non_dominated(y, neighbourhood):
+    for n in neighbourhood:
+        if n.does_dominate(y):
+            return False
+    return True
+
+
+def get_non_dominated(x):
+    nd = set()
+    for i in range(len(x)):
+        if is_non_dominated(x[i], x):
+            nd.add(x[i])
+    return list(nd)
 
 
 def solve_helper(parent_population, data):
-    ep = set()
     Log.log("timestamp")
     b = euclidean_distance_mapping(parent_population)
     Log.log("timestamp")
-    x = deepcopy(parent_population)
     for t in range(Constants.NUM_GENERATIONS):
         Log.log("Generation: " + str(t))
         for i in range(len(parent_population)):
-            y = generate_child(parent_population, data)
-            x.append(y)
+            y = generate_child([parent_population[i] for i in b[i]], data)
             neighbourhood = [parent_population[index] for index in b[i]]
-            for n in neighbourhood:
-                if n not in x:
-                    continue
-                if y.does_dominate(n):
-                    x.remove(n)
-                elif n.does_dominate(y):
-                    x.remove(y)
-                    break
-            x = list(set(x))
-            refresh_ep(ep, y)
-    return list(ep)
+            if is_non_dominated(y, neighbourhood):
+                parent_population[i] = y
+    return get_non_dominated(parent_population)
 
 
 class Moead(Solver):

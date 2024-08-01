4953b9e0f34ff9e2bd0d05dce1a704ac554b280c
grandpariley <rileyherman@duck.com>
2024-07-31 21:03:09 -0600

    random values actually do the thing. now let's step through a real example

--- pkg/random/random.py
+++ pkg/random/random.py
@@ -24,17 +24,14 @@ class Random:
 
     @classmethod
     def random_int_between_a_and_b(cls, a, b):
-        print(str(cls.test) + " | " + str(cls.non_random_values["random_int_between_a_and_b"]))
         if cls.test and cls.non_random_values["random_int_between_a_and_b"]:
             return cls.non_random_values["random_int_between_a_and_b"].pop()
-        print("random_int_between_a_and_b " + str(a) + " | " + str(b))
         return random.randint(a, b)
 
     @classmethod
     def random_choice(cls, lst):
         if cls.test and cls.non_random_values["random_choice"]:
             return cls.non_random_values["random_choice"].pop()
-        print("random_choice")
         if len(lst) > 0:
             return random.choice(lst)
         raise ValueError("no choice in list: " + str(lst))

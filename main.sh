0365793f03229b0d9f3cf2ffddfa164f7a0997f8
grandpariley <rileyherman@duck.com>
2024-08-13 12:08:06 -0600

    script

--- main.sh
+++ main.sh
@@ -4,12 +4,12 @@ for i
 do
 case $i in 
     test)
-#        load-env
         LOG_LEVEL='none' pipenv run python3 -m unittest discover
     ;;
     run)
-#        load-env
         PIPENV_VERBOSITY=-1 LOG_LEVEL='debug' pipenv run python3 main.py
+        PIPENV_VERBOSITY=-1 LOG_LEVEL='debug' pipenv run python3 validation.py
+        PIPENV_VERBOSITY=-1 LOG_LEVEL='debug' pipenv run python3 evaluation.py
     ;;
 esac
 done

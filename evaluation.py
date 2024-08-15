50b0a811cfc858334ecdfd322948d17e863a9afd
grandpariley <rileyherman@duck.com>
2024-08-15 14:52:19 -0600

    make the requisite tables

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,11 @@
+import csv
 import itertools
 import json
 import math
 import os.path
 
 import matplotlib.pyplot as plt
+from numpy.ma.extras import average
 
 from main import PROBLEMS
 from pkg.consts import Constants
@@ -12,9 +14,9 @@ ORDER_OF_COLOURS = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
 INDEX_TO_LABEL = ['risk', 'return', 'environment', 'governance', 'social']
 
 
-def flatten(generation, i=0):
+def flatten(solutions, i=0):
     max_objective = -math.inf
-    for solution in generation:
+    for solution in solutions:
         if solution['objectives'][i] > max_objective:
             max_objective = solution['objectives'][i]
     return max_objective
@@ -68,14 +70,54 @@ def get_solutions(name, run):
         return json.load(json_file)
 
 
-def main():
-    for run in range(Constants.NUM_RUNS):
-        for name in PROBLEMS.keys():
-            graph_generations(name, str(run), get_generations(name, run))
-            solutions = get_solutions(name, run)
-            if len(solutions[0]['objectives']) > 1:
-                graph_solution_bigraph(name, str(run), solutions)
+def calculate_one(solution, objective):
+    return sum([
+        value * Constants.DATA[name][objective] for name, value in solution['variables'].items()
+    ])
 
+
+def calculate_all(solutions, objective):
+    return max([
+        calculate_one(solution, objective) for solution in solutions
+    ])
+
+
+def calculate_average(solutions_by_run, objective):
+    return average([
+        calculate_all(solutions, objective) for solutions in solutions_by_run
+    ])
+
+
+def get_benchmark():
+    with open('index-data.json', 'r') as json_file:
+        return json.load(json_file)['^GSPTSE']
+
+
+def get_table_vs_benchmark(solutions_by_run):
+    benchmark = get_benchmark()
+    return [
+        ['return', calculate_average(solutions_by_run, 'return'), benchmark['return']],
+        ['var', calculate_average(solutions_by_run, 'var'), benchmark['var']],
+        ['cvar', calculate_average(solutions_by_run, 'cvar'), benchmark['cvar']],
+        ['environment', calculate_average(solutions_by_run, 'environment'), 'N/A'],
+        ['social', calculate_average(solutions_by_run, 'social'), 'N/A'],
+        ['governance', calculate_average(solutions_by_run, 'governance'), 'N/A']
+    ]
+
+
+def table_vs_benchmark(name, solutions_by_run):
+    with open(name + '/benchmark-comparison.csv', 'w') as csv_file:
+        csv.writer(csv_file).writerows(get_table_vs_benchmark(solutions_by_run))
+
+
+def main():
+    for name in PROBLEMS.keys():
+        # for run in range(Constants.NUM_RUNS):
+        #     graph_generations(name, str(run), get_generations(name, run))
+        #     solutions = get_solutions(name, run)
+        #     if len(solutions[0]['objectives']) > 1:
+        #         graph_solution_bigraph(name, str(run), solutions)
+        table_vs_benchmark(name, [get_solutions(name, run) for run in range(Constants.NUM_RUNS)])
 
 
 if __name__ == '__main__':

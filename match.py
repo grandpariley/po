8dc58e527cf4925e33f1ba3e651914250f06d214
grandpariley <rileyherman@duck.com>
2024-09-26 17:11:43 -0600

    wut

--- match.py
+++ match.py
@@ -17,7 +17,8 @@ def get_final_solutions_combined(problem):
 
 
 def get_value(solution, weights):
-    weights.pop('portfolio_id')
+    print(weights)
+    print(weights.values())
     if len(solution['objectives']) != len(weights.values()):
         raise ValueError('Illegal shape! ' + str(len(solution['objectives'])) + ' != ' + str(len(weights)))
     return sum([solution['objectives'][i] * list(weights.values())[i] for i in range(len(weights))])

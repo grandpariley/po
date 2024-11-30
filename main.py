6e7023c7807973bd1b21cf28bcb5f99067266a6a
grandpariley <rileyherman@duck.com>
2024-11-30 16:49:19 -0600

    Revert "MORE LOGGING"
    This reverts commit 8c37157b9348ff428a6750eae4df879d7762566a.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def get_solutions(problems):
         Log.log("Generating solutions for " + name, "generate")
         input_solutions[name] = generate_solutions_discrete_domain(problem)
         Log.log("Generating complete! Generated " + str(len(input_solutions[name])) + " solutions")
-        Log.log("For example, " + str(len(input_solutions[name][0])))
     output_solutions = {}
     for name in problems.keys():
         Log.log("Starting to solve using MOEA/D for " + name, name)
